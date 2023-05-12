169de933bc3c3cf874d3dc6cb980a7521b493757
Igor Czyż <igorczyz13@gmail.com>
2023-05-12 07:41:05 +0200

    function createToolsArea

--- script.js
+++ script.js
@@ -36,15 +36,16 @@ const prepareDOMEvents = () => {
 }
 
 const addNewTask = () => {
- if($todoInput.value !== '') {
-    $idNumber++;
-    $newTask = document.createElement('li');
-    $newTask.innerText = $todoInput.value;
-    $newTask.setAttribute('id', `todo-${$idNumber}`);
-    $ulList.appendChild($newTask);
+    if ($todoInput.value !== '') {
+        $idNumber++;
+        $newTask = document.createElement('li');
+        $newTask.innerText = $todoInput.value;
+        $newTask.setAttribute('id', `todo-${$idNumber}`);
+        $ulList.appendChild($newTask);
 
-    $todoInput.value = '';
-    $alertInfo.innerText = '';
+        $todoInput.value = '';
+        $alertInfo.innerText = '';
+        createToolsArea();
     } else {
         $alertInfo.innerText = 'Wpisz treść zadania!';
     }
@@ -54,6 +55,28 @@ const enterCheck = () => {
     if (event.keyCode === 13) {
         addNewTask();
     }
+}
+
+const createToolsArea = () => {
+    const toolsPanel = document.createElement('div');
+    toolsPanel.classList.add.('tools');
+    $newTask.appendChild(toolsPanel);
+
+    const completeBtn = document.createElement('button');
+    completeBtn.classList.add('complete');
+    completeBtn.innerHTML = '<i class="fas fa-check"></i>'
+
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('edit');
+    editBtn.innerHTML = 'EDIT';
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete');
+    deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+
+    toolsPanel.appendChild(completeBtn);
+    toolsPanel.appendChild(editBtn);
+    toolsPanel.appendChild(deleteBtn);
 }
 
 document.addEventListener('DOMContentLoaded', main);
